266bd121ab5ea1bbc174a38a28f6ad2dab17014c
Peter Sturm <sturm@uni-trier.de>
2023-12-13 11:44:17 +0100

    Correcting wrong file version of Table.java in last merge

--- src/Table.java
+++ src/Table.java
@@ -32,15 +32,9 @@ public class Table {
             queue.enqueue();
             seat = findAndOccupyEmptySeat();
         }
-        if (seat % 2 == 0) {
-            chopsticks[seat].take();
-            Thread.sleep(100); // to make deadlock more likely
-            chopsticks[(seat + 1) % size].take();
-        } else {
-            chopsticks[(seat + 1) % size].take();
-            Thread.sleep(100); // to make deadlock more likely
-            chopsticks[seat].take();
-        }
+        chopsticks[seat].take();
+        Thread.sleep(100); // to make deadlock more likely
+        chopsticks[(seat + 1) % size].take();
         return seat;
     }
 
